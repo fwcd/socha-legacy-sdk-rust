d00887e722e1588c13d4d3e9c9c8ee4ccc8b72b7
fwcd <fwcdmail@gmail.com>
2020-10-13 17:10:32 +0200

    Add efficient CoordinateSet representation

--- plugin-2021/src/game/piece_shape.rs
+++ plugin-2021/src/game/piece_shape.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, collections::HashSet, fmt, str::FromStr};
+use std::{collections::HashMap, fmt, str::FromStr};
 
 use lazy_static::lazy_static;
 use socha_client_base::{error::SCError, util::SCResult, xml_node::FromXmlNode, xml_node::XmlNode};
@@ -39,18 +39,114 @@ lazy_static! {
     };
 }
 
+const MAX_SIDE_LENGTH: i32 = 5;
+
+/// An efficient representation of a piece shape's normalized coordinates.
+/// Since every piece shape is less than 5x5 is size, we can represent it
+/// using a 5x5 bit-matrix:
+///
+///     +---+---+---+---+----+
+///     | 0 | 1 | 2 | 3 |  4 |
+///     +---+---+---+---+----+
+///     | 5 | 6 |            |
+///     +---+---+    ...     |
+///     |                    |
+///     +               +----+
+///     |               | 24 |
+///     +---+---+---+---+----+
+///
+/// These bits are stored in the right-end of of a 32-bit integer.
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+struct CoordinateSet {
+    bits: u32
+}
+
+impl CoordinateSet {
+    pub fn new() -> Self {
+        Self { bits: 0 }
+    }
+
+    fn index_of(coordinates: Coordinates) -> usize {
+        assert!(coordinates.x >= 0 && coordinates.y >= 0, "Coordinates have to be positive!");
+        assert!(coordinates.y < MAX_SIDE_LENGTH && coordinates.y < MAX_SIDE_LENGTH, "Coordinates are out of bounds!");
+
+        let i = (coordinates.y * MAX_SIDE_LENGTH) + coordinates.x;
+        i as usize
+    }
+
+    /// Inserts a pair of coordinates (inside the 5x5 box) into the set.
+    pub fn insert(&mut self, coordinates: Coordinates) {
+        self.bits |= 1 << Self::index_of(coordinates);
+    }
+
+    /// Checks whether the set contains a given pair of coordinates.
+    pub fn contains(&self, coordinates: Coordinates) -> bool {
+           coordinates.x >= 0
+        && coordinates.y >= 0
+        && coordinates.x < MAX_SIDE_LENGTH
+        && coordinates.y < MAX_SIDE_LENGTH
+        && ((self.bits >> Self::index_of(coordinates)) & 1) == 1
+    }
+}
+
+impl<I> From<I> for CoordinateSet where I: Iterator<Item=Coordinates> {
+    fn from(coordinates: I) -> Self {
+        let mut set = Self::new();
+
+        for coordinates in coordinates {
+            set.insert(coordinates);
+        }
+
+        set
+    }
+}
+
+struct CoordinateSetIterator {
+    bits: u32,
+    i: i32
+}
+
+impl Iterator for CoordinateSetIterator {
+    type Item = Coordinates;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.i < (MAX_SIDE_LENGTH * MAX_SIDE_LENGTH) {
+            let i = self.i;
+            let bits = self.bits;
+
+            self.bits >>= 1;
+            self.i += 1;
+
+            if (bits & 1) == 1 {
+                return Some(Coordinates::new(i % MAX_SIDE_LENGTH, i / MAX_SIDE_LENGTH));
+            }
+        }
+        
+        None
+    }
+}
+
+impl IntoIterator for CoordinateSet {
+    type Item = Coordinates;
+    type IntoIter = CoordinateSetIterator;
+
+    fn into_iter(self) -> Self::IntoIter {
+        CoordinateSetIterator { bits: self.bits, i: 0 }
+    }
+}
+
 /// Represents a shape in Blokus. There are 21 different kinds of these.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct PieceShape {
     /// The shape's internal name.
     name: String,
     /// The normalized coordinates that make up the shape.
-    coordinates: HashSet<Coordinates>
+    coordinates: CoordinateSet
 }
 
 impl PieceShape {
     fn new(name: &str, coordinates: impl IntoIterator<Item=Coordinates>) -> Self {
-        Self { name: name.to_owned(), coordinates: coordinates.into_iter().collect() }
+        Self { name: name.to_owned(), coordinates: CoordinateSet::from(coordinates.into_iter()) }
     }
 
     pub fn name(&self) -> &str {
@@ -59,30 +155,30 @@ impl PieceShape {
 
     /// A list of occupied fields, with the upper left corner being
     /// the origin (0, 0), the x-axis pointed right and the y-axis pointed downwards
-    pub fn coordinates(&self) -> &HashSet<Coordinates> {
-        &self.coordinates
+    pub fn coordinates(&self) -> impl Iterator<Item=Coordinates> {
+        self.coordinates.into_iter()
     }
 
     /// Mirrors this shape by negating all coordinates.
     fn mirror(&self) -> Self {
-        Self::new(self.name(), self.coordinates.iter().map(|&c| -c))
+        Self::new(self.name(), self.coordinates().map(|c| -c))
     }
 
     /// Turns this piece 90 degrees to the right.
     fn turn_right(&self) -> Self {
-        Self::new(self.name(), self.coordinates.iter().map(|c| c.turn_right()))
+        Self::new(self.name(), self.coordinates().map(|c| c.turn_right()))
     }
 
     /// Turns this piece 90 degrees to the left.
     fn turn_left(&self) -> Self {
-        Self::new(self.name(), self.coordinates.iter().map(|c| c.turn_left()))
+        Self::new(self.name(), self.coordinates().map(|c| c.turn_left()))
     }
 
     /// Adjusts the coordinates of this piece shape to be relative
     /// to its minimum coords.
     fn align(&self) -> Self {
-        let min_coords = self.coordinates.iter().fold(Coordinates::new(BOARD_SIZE as i32, BOARD_SIZE as i32), |m, &c| m.min(c));
-        Self::new(self.name(), self.coordinates.iter().map(|&c| c - min_coords))
+        let min_coords = self.coordinates().fold(Coordinates::new(BOARD_SIZE as i32, BOARD_SIZE as i32), |m, c| m.min(c));
+        Self::new(self.name(), self.coordinates().map(|c| c - min_coords))
     }
 
     /// Performs a rotation of this piece shape.
