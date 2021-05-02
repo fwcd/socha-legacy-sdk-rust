9debcf4b14a5e77cc57b658c7c8246383367307f
fwcd <fwcdmail@gmail.com>
2021-05-03 00:41:55 +0200

    Add mock plugin

--- client-base/src/plugin.rs
+++ client-base/src/plugin.rs
@@ -3,7 +3,7 @@ use std::fmt::Debug;
 use crate::util::HasOpponent;
 use crate::error::SCError;
 
-/// An "type family" trait that defines types
+/// A "type family" trait that defines types
 /// which represent various parts of a game.
 pub trait SCPlugin: Debug {
     type PlayerColor: Copy + Debug + Eq + HasOpponent + FromStr<Err=SCError>;
@@ -30,4 +30,74 @@ pub trait HasPlayerColor {
 pub trait HasTurn {
     /// Fetches the turn.
     fn turn(&self) -> u32;
+}
+
+/// A mock implementation of a plugin for testing.
+#[derive(Debug)]
+pub struct MockPlugin;
+
+/// A mock player color for testing.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum MockPlayerColor {
+    Red,
+    Blue,
+}
+
+/// A mock player for testing.
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct MockPlayer;
+
+/// A mock game state for testing.
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct MockGameState {
+    player_color: MockPlayerColor,
+    turn: u32,
+}
+
+/// A mock move for testing.
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct MockMove;
+
+impl SCPlugin for MockPlugin {
+    type PlayerColor = MockPlayerColor;
+    type Player = MockPlayer;
+    type GameState = MockGameState;
+    type Move = MockMove;
+
+    fn protocol_game_type<'a>() -> &'a str { "mock" }
+}
+
+impl FromStr for MockPlayerColor {
+    type Err = SCError;
+
+    fn from_str(s: &str) -> Result<Self, SCError> {
+        match s {
+            "RED" => Ok(Self::Red),
+            "BLUE" => Ok(Self::Blue),
+            _ => Err(SCError::Custom(format!("Invalid mock player color: {}", s))),
+        }
+    }
+}
+
+impl HasOpponent for MockPlayerColor {
+    fn opponent(self) -> Self {
+        match self {
+            Self::Red => Self::Blue,
+            Self::Blue => Self::Red,
+        }
+    }
+}
+
+impl HasTurn for MockGameState {
+    fn turn(&self) -> u32 {
+        self.turn
+    }
+}
+
+impl HasPlayerColor for MockGameState {
+    type PlayerColor = MockPlayerColor;
+
+    fn player_color(&self) -> MockPlayerColor {
+        self.player_color
+    }
 }
