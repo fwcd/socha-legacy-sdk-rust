3bce1118a31638ac318c8ed53ae008022f3cd9fb
fwcd <fwcdmail@gmail.com>
2021-05-02 23:59:04 +0200

    Add tests ensuring that moves get serialized correctly

--- plugin-2021/src/game/move.rs
+++ plugin-2021/src/game/move.rs
@@ -28,3 +28,35 @@ impl Move {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Move;
+    use crate::game::{Color, PIECE_SHAPES_BY_NAME, Piece, Rotation, Vec2};
+    use quick_xml::se::to_string;
+
+    #[test]
+    fn test_serialization() {
+        let set_move = Move::Set {
+            piece: Piece {
+                kind: PIECE_SHAPES_BY_NAME["TRIO_L"].clone(),
+                rotation: Rotation::Mirror,
+                is_flipped: true,
+                color: Color::Blue,
+                position: Vec2::new(2, 3),
+            }
+        };
+        assert_eq!(
+            to_string(&set_move).unwrap().as_str(),
+            r#"<data class="sc.plugin2021.SetMove"><piece kind="TRIO_L" rotation="MIRROR" isFlipped="true" color="BLUE"><position x="2" y="3"/></piece></data>"#
+        );
+
+        let skip_move = Move::Skip {
+            color: Color::Red
+        };
+        assert_eq!(
+            to_string(&skip_move).unwrap().as_str(),
+            r#"<data class="sc.plugin2021.SkipMove"><color>RED</color></data>"#
+        )
+    }
+}
